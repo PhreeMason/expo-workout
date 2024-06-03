977231fa7d993112c94a15f1f3999d58243623c7
phreemason <mr.omarmason@gmail.com>
2024-06-03 14:04:29 -0400

    added expo router

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,15 +1,12 @@
-import { StyleSheet, Text, View,  } from "react-native";
-
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
+import { StyleSheet, Text, View } from "react-native";
 
 function ExerciseListItem({ exercise }) {
   return (
     <View style={styles.exerciseContainer}>
       <Text style={styles.exerciseName}>{exercise.name}</Text>
       <Text style={styles.exerciseSubtitle}>
-        {capitalize(exercise.muscle)} | {capitalize(exercise.equipment)}
+        <Text style={styles.subValue}>{exercise.muscle}</Text> |{" "}
+        <Text style={styles.subValue}>{exercise.equipment}</Text>
       </Text>
     </View>
   );
@@ -21,6 +18,18 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     gap: 5,
+    marginHorizontal: 2,
+
+    // shadow
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
   },
   exerciseName: {
     fontSize: 20,
@@ -29,7 +38,9 @@ const styles = StyleSheet.create({
   exerciseSubtitle: {
     color: "dimgray",
   },
+  subValue: {
+    textTransform: "capitalize",
+  },
 });
-
 
 export default ExerciseListItem;
