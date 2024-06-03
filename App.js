084200b4d23280e2b57f6feebebf69e34f0cf0d1
phreemason <mr.omarmason@gmail.com>
2024-06-03 13:01:02 -0400

    rendered and styled one exercise

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import exercises from './assets/data/exercises.json';
 
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export default function App() {
   const exercise = exercises[0];
   return (
     <View style={styles.container}>
+      <View style={styles.exerciseContainer}>
+
       <Text style={styles.exerciseName}>{exercises[0].name}</Text>
       <Text style={styles.exerciseSubtitle}>
-        Muscle: {exercises[0].muscle} | Equipment: {exercises[0].equipment}
-        </Text>
+        {capitalize(exercises[0].muscle)} | {capitalize(exercises[0].equipment)}
+      </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,9 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding: 10,
+  },
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
   },
   exerciseName: {
     fontSize: 20,
